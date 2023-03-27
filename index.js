79ab43bf6e1d6bbf90dd33cda99c1c2677b08632
Ankur Attri <84219364+ankur874@users.noreply.github.com>
2023-03-27 20:58:30 +0530

    image

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ const app = express();
 })();
 
 app.use(express.json({ extended: false }));
-app.use(express.static(__dirname + "/images"));
-app.use("./images", express.static("images"));
+app.use("/images", express.static(path.join(__dirname, "./images")));
 app.use("/api/users", require("./Routes/UserRoutes"));
 app.use("/api/properties", require("./Routes/PropertyRoutes"));
 app.use("/api/requests", require("./Routes/RequestRoutes"));
