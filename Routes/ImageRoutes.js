c2f70590d81e0f0fd5f453f64b9459af0b84cb22
Ankur Attri <84219364+ankur874@users.noreply.github.com>
2023-03-27 20:40:10 +0530

    image

--- Routes/ImageRoutes.js
+++ Routes/ImageRoutes.js
@@ -6,18 +6,22 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "images/");
+    cb(null, "./images");
   },
   filename(req, file, cb) {
-    cb(null, `${Date.now()}`);
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
   },
 });
 
 const upload = multer({ storage });
 
 router.post("/", upload.single("image"), (req, res) => {
+  // res.send(`/${req.file.path.replace(/\\/g, '/')}`);
   res.status(200).json({
-    name: `${req.file.path}`,
+    name: `/${req.file.path.replace(/\\/g, "/")}`,
   });
 });
 
